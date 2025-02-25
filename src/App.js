634be1f78082be31d30854ba426bf29c773852d9
sydv-ys <Aysu.Seyidova@Kibrit.Tech>
2025-02-25 11:55:56 +0400

    pose request

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Title from "./components/Title";
 import First from "./examples/1-first-request";
 import Headers from "./examples/2-headers";
+import Post from "./examples/3-post-request";
+
+
 function App() {
   return (
     <main>
       {/* <Title /> */}
       {/* <First /> */}
-      <Headers />
+      {/* <Headers /> */}
+      <Post/>
     </main>
   );
 }
